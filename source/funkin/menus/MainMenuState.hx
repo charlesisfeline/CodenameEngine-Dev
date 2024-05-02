bcd98402f3b07ae4a0c45ad0b4c5e9006715dc95
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2024-05-02 16:59:27 +0200

    better sprites

--- source/funkin/menus/MainMenuState.hx
+++ source/funkin/menus/MainMenuState.hx
@@ -158,7 +158,7 @@ class MainMenuState extends MusicBeatState
 			{
 				case 'story mode': FlxG.switchState(new StoryMenuState());
 				case 'freeplay': FlxG.switchState(new FreeplayState());
-				case 'donate': FlxG.switchState(new CreditsMain());
+				case 'credits': FlxG.switchState(new CreditsMain());
 				case 'options': FlxG.switchState(new OptionsMenu());
 			}
 		});
